a9cc1468fd3d325302e6c0b1c647cb779d5378e9
AidanPolese <aidan.polese@gmail.com>
2016-11-24 22:24:39 -0500

    FINAL
    FINAL

--- q3iii.c
+++ q3iii.c
@@ -1,4 +1,9 @@
 #include "mystring.h"
+//Manvir Grewal : 10193283 
+//Aidan Polese : 10197342 
+//Kathleen Abols : 10179548 
+//Lianne Orlowski : 10204124
+
 int main() {
     char a[] = "cat";
     char b[] = "dog";
