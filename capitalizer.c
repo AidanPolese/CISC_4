f990dcbce76002cad66fa4ac84928d371cffb1a9
LianneO <15lkso@queensu.ca>
2016-11-25 20:23:05 -0500

    FINAL

--- capitalizer.c
+++ capitalizer.c
@@ -16,9 +16,6 @@ int main(int argc, char *argv[]) {
 	char previousLetter=' ';
 	char c;
 	int counter=0;
-//	int i;
-//	char buff[255];
-//	char* token;
 			
 	//checks to see if user provided file as a command line argument
 	if (argc < 2) {
@@ -32,30 +29,19 @@ int main(int argc, char *argv[]) {
 		printf("Error opening file\n");
 		return 1;
 	}
-//	while(1) {
-//		fgets(buff, 255, filePtr);
-//		if (feof(filePtr)){
-//			break;
-//		}
-//		token = strtok(buff, " ");
-//		for (i=0; buff[i] != '\0'; i++){
-//			if(isalpha(buff[i]) && (buff[i-1] == ' ')) {
-//				buff[i] = toupper(buff[i]);	
-//			}
-//		}
-//	}
-//	fclose(filePtr);
-//	return(0);
-
 
 	while((c = fgetc(filePtr)) != EOF){
 		if (c=='\n'){
 			counter=0;
 			putchar('\n');
 		}
+		//checks if the previous character is not in the alphabet (space)
+		//and if counter of spaces is even (to get every other word)
+		//then puts that character to upper case
 		else if(!isalpha(previousLetter) && (counter%2)==0){
 			putchar(toupper(c));
 		}
+		// otherwise keep same character
 		else{
 			putchar(c);
 		}
